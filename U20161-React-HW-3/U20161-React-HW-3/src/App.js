9b0743d69a372e18b67591fc1dc8706d76528c6e
Roman <48173785+Rubysfarms@users.noreply.github.com>
2020-09-27 18:42:02 +0300

    hw

---
+++ U20161-React-HW-3/U20161-React-HW-3/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalState from './context/GlobalState';
+import ProductsPage from './pages/Products';
+import CartPage from './pages/Cart';
+
+class App extends Component {
+  render() {
+    return (
+      <GlobalState>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ProductsPage} exact />
+            <Route path="/cart" component={CartPage} exact />            
+          </Switch>
+        </BrowserRouter>
+      </GlobalState>
+    );
+  }
+}
+
+export default App;
